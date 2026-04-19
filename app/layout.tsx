223c842efd135825f1b7f08e034c834bb0b17657
Truong Giang <truong.giang@thoughtworks.com>
2026-04-19 13:04:18 +0700

    Refactor global styles to disable overscroll behavior and optimize layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${manrope.variable} ${inter.variable} ${materialSymbols.variable} h-full antialiased`}>
+    <html lang="en" className={`${manrope.variable} ${inter.variable} ${materialSymbols.variable} antialiased`}>
       <body className="min-h-screen flex bg-surface-dim">
         <SidebarProvider>
           <Sidebar />
